2147d111f5c6c171e83ef00d4e22c3497ff9a894
biaoge <15321660372@163.com>
2024-12-09 12:03:17 +0800

    ...

--- EECardlib/src/main/java/com/easyeuro/eecardlib/utils/EECardManager.kt
+++ EECardlib/src/main/java/com/easyeuro/eecardlib/utils/EECardManager.kt
@@ -260,26 +260,26 @@ class EECardManager(
     }
 
     private val serviceRSAExponent = "0x10001".toByteArray()
-    private val serviceRSAModulus =
-        "00d1d5530fb00fd3bdc08ca39102aa09a1e8e01d1cda814d67d58ce6c3f93d07446c56777e32e7da369b6d5ab432dc44d84ed4f29725e27853adce6a14bbbd745202e1344a1a8b47ff43735dde51e8234b53407018f35bac862a3129d39e02d373d94b1b2f59469fa343b13bde36f65f17d8d2955ee37f0684779062d04f59f61d6e5f4452f0942b80faef401c9cc06541e4a8a595a429fc0a76014837825cf2e571057eed6a85ce934a99e03bea9975451200df0d7cf3e757e9d0506b8873c80cff0c3657b97588f15f551985b142b6f462b87257ad8bd750cc220d7c47b328cf17cc2bbfc0edcd99a85d85f224aef47b3e7062b1a30f31b63853375cbe0251df".toByteArray()
     private val serviceURLPrd = "https://client-api.d1.thalescloud.io/"
     private val digitalCardURLPrd = "https://hapi.dbp.thalescloud.io/mg/tpc/"
     private val issuerID = "is_cko_oui"
 
     /**
+     *
      * Add card to google wallet
-     * PRODUCTION only
-     * @param cardholderId Start with "crh_"
+     * PRODUCTION environment only
+     *
      * */
     fun provision(
         activity: android.app.Activity,
         cardholderId: String,
         provisionToken: String,
+        serviceRSAModulus:String,
         completionHandler: ValuelessCompletion
     ) {
         val configuration = ProvisioningConfiguration(
             serviceRSAExponent = serviceRSAExponent,
-            serviceRSAModulus = serviceRSAModulus,
+            serviceRSAModulus = serviceRSAModulus.toByteArray(),
             serviceURL = serviceURLPrd,
             digitalCardURL = digitalCardURLPrd,
             issuerID = issuerID
